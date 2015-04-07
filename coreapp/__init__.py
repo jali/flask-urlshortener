8b186ddf5c80b583c614db3035c6a898bf55045c
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2015-04-08 00:30:11 +0100

    amend

--- coreapp/__init__.py
+++ coreapp/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/local/bin/python
-# -*- coding: utf-8 -*-
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
--- coreapp/forms.py
+++ coreapp/forms.py
@@ -7,4 +7,3 @@ from wtforms.validators import DataRequired, url, Optional
 
 class ShortForm(Form):
 	long_url = TextField('long_url', validators=[DataRequired(), url(), Optional(strip_whitespace=True)])
-
