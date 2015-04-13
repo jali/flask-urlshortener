8683e30dea94bef4594a22f52334e34a48314d3c
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2015-04-13 10:08:10 +0100

    run tests in memory

--- config.py
+++ config.py
@@ -25,9 +25,10 @@ class DevelopmentConfig(Config):
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'urls.db')
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/urls'
 
+
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'urls.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 
 
 
