979ba5558e6f8c73e4a1cc47057f9d8a1bb16f85
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2015-04-13 10:10:19 +0100

    run tests against test database

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,13 +4,12 @@ import unittest
 from base import BaseTestCase
 
 class AppTestCase(BaseTestCase):
-	# Ensure that flask was set up correctly
 	def test_index(self):
 		response = self.client.get('/', content_type='html/text')
 		self.assertEqual(response.status_code, 200)
 
 	def test_page_loads(self):
-		response = self.client.get('/', content_type='html/text')
+		response = self.client.get('/')
 		self.assertIn(b'Your links', response.data)
 
 
